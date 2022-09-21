5182a25037fdbce78db2120492d3a21bf63627ea
Josh Moore <joshuamoore5899@gmail.com>
2022-09-21 12:23:38 -0600

    added sort and filter

--- controllers/home.js
+++ controllers/home.js
@@ -6,27 +6,9 @@ module.exports = {
     let aquariums = await Aquarium.find().sort({dateCreated: 'desc'}).lean();
     res.render('index.ejs', { aquariums: aquariums });
   },
-  getOld: async (req, res) => {
-    let aquariums = await Aquarium.find().sort({dateCreated: 'asc'}).lean();
-    res.render('index.ejs', { aquariums: aquariums });
-  },
-  getLiked: async (req, res) => {
-    let aquariums = await Aquarium.find().sort({likes: 'desc'}).lean();
-    res.render('index.ejs', { aquariums: aquariums });
-  },
-  getInspired: async (req, res) => {
-    let aquariums = await Aquarium.find().sort({inspired: 'desc'}).lean();
-    res.render('index.ejs', { aquariums: aquariums });
-  },
-  getBig: async (req, res) => {
-    let aquariums = await Aquarium.find().sort({trueSize: 'desc'}).lean();
-    res.render('index.ejs', { aquariums: aquariums });
-  },
-  getSmall: async (req, res) => {
-    let aquariums = await Aquarium.find().sort({trueSize: 'asc'}).lean();
-    res.render('index.ejs', { aquariums: aquariums });
-  },
   filterHome: async (req, res) => {
+
+    //get current date
     let date = new Date().toLocaleDateString()
     let month = date.slice(0, date.indexOf('/'));
     if (month < 10) {
@@ -37,6 +19,8 @@ module.exports = {
       day = `0${day}`;
     }
     let year = date.slice(date.lastIndexOf('/') + 1, date.length);
+
+    //get date to compare based off of current date and input date
     if (req.body.filterDate === 'all') {
       date = '2000-01-01'
     }
@@ -84,6 +68,8 @@ module.exports = {
       }
       date = `${year}-${month}-${day}`;
     }
+
+    //determine water type
     let waterType = "";
     if (req.body.filterWaterType === 'fresh') {
       waterType = 'fresh';
@@ -91,18 +77,167 @@ module.exports = {
     else if (req.body.filterWaterType === 'salt') {
       waterType = 'salt';
     }
+
     let aquariums;
-    if (waterType.length > 1) {
-      aquariums = await Aquarium.find({
-        waterType: waterType,
-        dateCreated: { $gte: date },
-      }).lean()
+
+    //divide based on sort criteria and get aquarium that matches requirements
+    if (req.body.selectSortBy === 'new') {
+      if (waterType.length > 1) {
+        if (req.body.filterFish !== 'all') {
+          console.log('there')
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ dateCreated: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).sort({ dateCreated: 'desc' }).lean()
+        }
+
+      }
+      else {
+        if (req.body.filterFish !== 'all') {
+          console.log('here')
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ dateCreated: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).sort({ dateCreated: 'desc' }).lean()
+        }
+      }
     }
-    else {
-      aquariums = await Aquarium.find({
-        dateCreated: { $gte: date },
-      }).lean()
+    else if (req.body.selectSortBy === 'old') {
+      if (waterType.length > 1) {
+        if (req.body.filterFish !== 'all') {
+          console.log('there')
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ dateCreated: 'asc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).sort({ dateCreated: 'asc' }).lean()
+        }
+
+      }
+      else {
+        if (req.body.filterFish !== 'all') {
+          console.log('here')
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ dateCreated: 'asc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).sort({ dateCreated: 'asc' }).lean()
+        }
+      }
     }
+    else if (req.body.selectSortBy === 'liked') {
+      if (waterType.length > 1) {
+        if (req.body.filterFish !== 'all') {
+          console.log('there')
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ likes: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).sort({ likes: 'desc' }).lean()
+        }
+
+      }
+      else {
+        if (req.body.filterFish !== 'all') {
+          console.log('here')
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ likes: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).sort({ likes: 'desc' }).lean()
+        }
+      }
+    }
+    else if (req.body.selectSortBy === 'big') {
+      if (waterType.length > 1) {
+        if (req.body.filterFish !== 'all') {
+          console.log('there')
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ trueSize: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).sort({ trueSize: 'desc' }).lean()
+        }
+
+      }
+      else {
+        if (req.body.filterFish !== 'all') {
+          console.log('here')
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ trueSize: 'desc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).sort({ trueSize: 'desc' }).lean()
+        }
+      }
+    }
+    else if (req.body.selectSortBy === 'small') {
+      if (waterType.length > 1) {
+        if (req.body.filterFish !== 'all') {
+          console.log('there')
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ trueSize: 'asc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            waterType: waterType,
+            dateCreated: { $gte: date },
+          }).sort({ trueSize: 'asc' }).lean()
+        }
+
+      }
+      else {
+        if (req.body.filterFish !== 'all') {
+          console.log('here')
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).find({ fish: req.body.filterFish }).sort({ trueSize: 'asc' }).lean()
+        }
+        else {
+          aquariums = await Aquarium.find({
+            dateCreated: { $gte: date },
+          }).sort({ trueSize: 'asc' }).lean()
+        }
+      }
+    }
+
+    //render page based on filter criteria
     res.render('index.ejs', { aquariums: aquariums });
   },
   addLike: async (req, res)=> {
--- routes/home.js
+++ routes/home.js
@@ -5,11 +5,6 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 router.get('/', homeController.getNew);
 router.post('/filter', homeController.filterHome);
-router.get('/new', homeController.getNew);
-router.get('/old', homeController.getOld);
-router.get('/liked', homeController.getLiked);
-router.get('/big', homeController.getBig);
-router.get('/small', homeController.getSmall);
 router.put('/addLike', homeController.addLike);
 
 module.exports = router;
