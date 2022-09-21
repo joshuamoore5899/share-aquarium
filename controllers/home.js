dd1acaa7b66f0c2f955f3e3829477c9a6454fa84
Josh Moore <joshuamoore5899@gmail.com>
2022-09-20 21:39:21 -0600

    added filter

--- controllers/home.js
+++ controllers/home.js
@@ -26,6 +26,85 @@ module.exports = {
     let aquariums = await Aquarium.find().sort({trueSize: 'asc'}).lean();
     res.render('index.ejs', { aquariums: aquariums });
   },
+  filterHome: async (req, res) => {
+    let date = new Date().toLocaleDateString()
+    let month = date.slice(0, date.indexOf('/'));
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    let day = date.slice(date.indexOf('/') + 1, date.lastIndexOf('/'));
+    if (day < 10) {
+      day = `0${day}`;
+    }
+    let year = date.slice(date.lastIndexOf('/') + 1, date.length);
+    if (req.body.filterDate === 'all') {
+      date = '2000-01-01'
+    }
+    else if (req.body.filterDate === 'year') {
+      year = Number(year) - 1;
+      date = `${year}-${month}-${day}`;
+    }
+    else if (req.body.filterDate === '3months') {
+      month = Number(month) - 3;
+      if (Number(month) < 1) {
+        year = Number(year) - 1;
+        month = 12 + Number(month);
+      }
+      date = `${year}-${month}-${day}`;
+    }
+    else if (req.body.filterDate === '1month') {
+      month = Number(month) - 1;
+      if (Number(month) < 1) {
+        year = Number(year) - 1;
+        month = 12 + Number(month);
+      }
+      date = `${year}-${month}-${day}`;
+    }
+    else if (req.body.filterDate === '1week') {
+      day = Number(day) - 7;
+      if (Number(day) < 1) {
+        month = Number(month) - 1;
+        if (Number(month) < 1) {
+          year = Number(year) - 1;
+          month = 12 + Number(month);
+        }
+        day = 30 + Number(day);
+      }
+      date = `${year}-${month}-${day}`;
+    }
+    else if (req.body.filterDate === '24hours') {
+      day = Number(day) - 1;
+      if (Number(day) < 1) {
+        month = Number(month) - 1;
+        if (Number(month) < 1) {
+          year = Number(year) - 1;
+          month = 12 + Number(month);
+        }
+        day = 30 + Number(day);
+      }
+      date = `${year}-${month}-${day}`;
+    }
+    let waterType = "";
+    if (req.body.filterWaterType === 'fresh') {
+      waterType = 'fresh';
+    }
+    else if (req.body.filterWaterType === 'salt') {
+      waterType = 'salt';
+    }
+    let aquariums;
+    if (waterType.length > 1) {
+      aquariums = await Aquarium.find({
+        waterType: waterType,
+        dateCreated: { $gte: date },
+      }).lean()
+    }
+    else {
+      aquariums = await Aquarium.find({
+        dateCreated: { $gte: date },
+      }).lean()
+    }
+    res.render('index.ejs', { aquariums: aquariums });
+  },
   addLike: async (req, res)=> {
     try {
       const aquarium = await Aquarium.findOne({_id:req.body.itemID}).lean();
--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/home');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 router.get('/', homeController.getNew);
+router.post('/filter', homeController.filterHome);
 router.get('/new', homeController.getNew);
 router.get('/old', homeController.getOld);
 router.get('/liked', homeController.getLiked);
