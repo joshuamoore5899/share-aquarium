6f1dfc85ced54e5c778aabdc47f242f585c3d515
Josh Moore <joshuamoore5899@gmail.com>
2022-10-31 20:06:11 -0600

    moved listen function

--- config/database.js
+++ config/database.js
@@ -6,6 +6,11 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
+    .then(
+      app.listen(process.env.PORT, ()=> {
+        console.log(`Server is running on port ${process.env.PORT}`);
+      })
+    )
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
     console.log('There it is')
